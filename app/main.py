4dc5a17b65c279b81b03e4588454179d5fcf8672
Mahmut Altun <maltun.7649@gmail.com>
2026-03-29 01:37:41 +0100

    frontend test - basic navigation (not final)

--- app/main.py
+++ app/main.py
@@ -1,10 +1,32 @@
 from nicegui import ui
 
+def show_dashboard():
+    content.clear()
+    with content:
+        ui.label('Dashboard Page')
+
+def show_products():
+    content.clear()
+    with content:
+        ui.label('Products Page')
+
+def show_add_product():
+    content.clear()
+    with content:
+        ui.label('Add Product Page')
+
+# ÜST SABİT ALAN
 ui.label('Inventory Management App')
 
 with ui.row():
-    ui.button('Dashboard')
-    ui.button('Products')
-    ui.button('Add Product')
+    ui.button('Dashboard', on_click=show_dashboard)
+    ui.button('Products', on_click=show_products)
+    ui.button('Add Product', on_click=show_add_product)
+
+# ALT İÇERİK ALANI
+content = ui.column()
+
+# başlangıç
+show_dashboard()
 
 ui.run()
