deeb20bd3434f7c15e384ab56200f01c518821a1
Aydin Ada <aydin.ada@students.fhnw.ch>
2026-05-08 12:00:27 +0200

    Update product_service.py

--- app/services/product_service.py
+++ app/services/product_service.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from sqlmodel import Session, select
 
-from app.models import Category, Location, Product
+from app.models import Category, StorageLocation, Product
 
 
 class ProductService:
