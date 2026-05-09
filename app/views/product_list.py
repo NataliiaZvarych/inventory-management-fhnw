e6ee7d71e47feb2a034819f913f271a340c0c70e
Nataliia Zvarych <nataliia.zvarych@students.fhnw.ch>
2026-05-09 08:28:22 +0200

    Delete app/views/product_list.py

--- app/views/product_list.py
+++
@@ -1,6 +0,0 @@
-# Product list view - placeholder for future UI
-from nicegui import ui
-
-def show_product_list():
-    ui.label("📦 Product List")
-    ui.label("Here you will see all products in the inventory.")
