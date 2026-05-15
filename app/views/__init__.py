e4294c7ce17d7cc33558235123934127eeadb2c9
Nataliia Zvarych <nataliia.zvarych@students.fhnw.ch>
2026-05-15 11:39:09 +0200

    fix main.py

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,7 +3,7 @@ from app.views.dashboard import dashboard_page
 from app.views.locations import locations_page
 from app.views.login import login_page
 from app.views.movements import movements_page
-from app.views.products.products_page import products_page
+
 from app.views.users import users_page
 
 __all__ = [
@@ -12,6 +12,5 @@ __all__ = [
 	"locations_page",
 	"login_page",
 	"movements_page",
-	"products_page",
 	"users_page",
 ]
--- app/views/products/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.views.products.add_product import render_add_product_form
-from app.views.products.product_list import render_product_list
-from app.views.products.products_page import products_page
-
-__all__ = ["render_add_product_form", "render_product_list", "products_page"]
